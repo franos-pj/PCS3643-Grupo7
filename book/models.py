1aaedde808364c51739a50fdac9ced651dfa7899
Francisco Mariani <francisco.mariani@polijunior.com.br>
2022-10-06 17:31:30 -0300

    FEAT: new route class, and initial creation tests

--- book/models.py
+++ book/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 
+# Classe Rota
+class Route(models.Model):
+    flightCode = models.CharField(max_length=20, primary_key=True)
+    airline = models.CharField(max_length=20, null=False)
+    departureAirport = models.CharField(max_length=3, null=False)
+    arrivalAirport = models.CharField(max_length=3, null=False)
+    scheduledTime = models.TimeField(null=False)
+
+    class Meta:
+        db_table = 'rotas'
+
 # Classe Voo
 class Flight(models.Model):
-    pass
+    flightId = models.BigAutoField(primary_key=True)
+    flightCode = models.ForeignKey(Route, on_delete=models.CASCADE)
+    scheduledDate = models.DateField(null=False)
+    realDate = models.DateField(null=True)
+    realTime = models.TimeField(null=True)
+    status = models.CharField(max_length=20, null=True)
+
+    class Meta:
+        db_table = 'voos'
--- book/tests.py
+++ book/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+import datetime
 
-# Create your tests here.
+# Test Models
+from models import Flight, Route
+
+class FlightModelTest(TestCase):
+
+    @classmethod
+    def test_creation(self):
+        Route.objects.create(
+            flightCode='AF0443',
+            airline='Air France', 
+            departureAirport = 'FLL',
+            arrivalAirport = 'CDG', 
+            scheduledTime = datetime.time(10, 30, 0)
+        )
+
+        Flight.objects.create(
+            flightCode='AF0443',
+            scheduledDate = datetime.date(year=2022, month=11, day=26),
+        )
+
+        flight_1 = Flight.objects.get(flightCode='AF0443')
+        self.assertEqual(flight_1.scheduledDate, datetime.date(year=2022, month=11, day=26))
