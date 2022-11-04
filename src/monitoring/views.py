3d865368266b683972ecae4f95a53ae629418ae1
LPFreitas <lfreitasp2001@gmail.com>
2022-11-04 11:01:23 -0300

    ADD: flightRegistration

--- src/monitoring/views.py
+++ src/monitoring/views.py
@@ -7,89 +7,86 @@ from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
 
 def dashboard(request):
-    return render(request, 'Dashboard.html')
+    return render(request, "Dashboard.html")
+
 
 def flightInfo(request):
-    return render(request, 'flight-info.html')
+    return render(request, "flight-info.html")
+
 
 def chooseReport(request):
-    return render(request, 'choose-report.html')
+    return render(request, "choose-report.html")
+
 
 def generalReport(request):
-    return render(request, 'general-report.html')
+    return render(request, "general-report.html")
+
 
 def specificReport(request):
-    return render(request, 'specific-report.html')
+    return render(request, "specific-report.html")
+
 
 def routesAndFlights(request):
-    return render(request, 'routes-and-flights.html')
+    return render(request, "routes-and-flights.html")
+
 
 def routesRecords(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.POST
-        flightCode = data['flightCode']
+        flightCode = data["flightCode"]
         if Route.objects.filter(flightCode=flightCode).exists():
-            redirectPath = 'routes-records/info/'+ flightCode + "/"
-            response = {
-                'success': True,
-                'id': flightCode,
-                'redirectPath': redirectPath
-            }
+            redirectPath = "routes-records/info/" + flightCode + "/"
+            response = {"success": True, "id": flightCode, "redirectPath": redirectPath}
             return HttpResponse(json.dumps(response))
         else:
-            response = {
-                'success': False,
-                'id': flightCode,
-                'redirectPath': None
-            }
+            response = {"success": False, "id": flightCode, "redirectPath": None}
             return HttpResponse(json.dumps(response))
     else:
-        return render(request, 'routes-records.html')
+        return render(request, "routes-records.html")
+
 
 @csrf_exempt
 def routeInfo(request, flightCode):
-    context ={}
+    context = {}
     print(request)
     route = get_object_or_404(Route, pk=flightCode)
-    form = RouteForm(request.POST or None, instance = route)
-    
-    if (request.method == 'POST'):
+    form = RouteForm(request.POST or None, instance=route)
+
+    if request.method == "POST":
         if form.is_valid():
             form.save()
             response = {
-                'id': form.cleaned_data["flightCode"],
-                'success': True,
-                'error': None
+                "id": form.cleaned_data["flightCode"],
+                "success": True,
+                "error": None,
             }
             return HttpResponse(json.dumps(response))
         else:
             response = {
-                'id': form.cleaned_data["flightCode"],
-                'success': False,
-                'error': form.errors.as_json()
+                "id": form.cleaned_data["flightCode"],
+                "success": False,
+                "error": form.errors.as_json(),
             }
             return HttpResponse(json.dumps(response))
 
-    elif (request.method == 'DELETE'):
+    elif request.method == "DELETE":
         deletion = route.delete()
         print(deletion)
-        response = {
-            'id': flightCode,
-            'success': True
-        }
+        response = {"id": flightCode, "success": True}
         return HttpResponse(json.dumps(response))
-            
 
-    context['route']= route
-    context['id'] = flightCode
-    context['form'] = form
-    return render(request, 'route-info.html', context)
+    context["route"] = route
+    context["id"] = flightCode
+    context["form"] = form
+    return render(request, "route-info.html", context)
+
 
 def routeRegistration(request):
-    context ={}
+    context = {}
 
     # create object of form
     form = RouteForm(request.POST or None, request.FILES or None)
@@ -99,32 +96,67 @@ def routeRegistration(request):
         # save the form data to model
         form.save()
         response = {
-            'id': form.cleaned_data["flightCode"],
-            'success': True,
-            'error': None
-            }
+            "id": form.cleaned_data["flightCode"],
+            "success": True,
+            "error": None,
+        }
         print("form valid")
-        print('myresponse', response)
+        print("myresponse", response)
         return HttpResponse(json.dumps(response))
-    elif request.method == 'POST':
+    elif request.method == "POST":
         print("form invalid", form.data)
         response = {
-            'id': form.data["flightCode"],
-            'success': False,
-            'error': form.errors.as_json()
-            }
-        print('myresponse', response)
-        return HttpResponse(json.dumps(response)) 
+            "id": form.data["flightCode"],
+            "success": False,
+            "error": form.errors.as_json(),
+        }
+        print("myresponse", response)
+        return HttpResponse(json.dumps(response))
 
-    context['form']= form
+    context["form"] = form
 
-    return render(request, 'route-registration.html', context)
+    return render(request, "route-registration.html", context)
+
 
 def flightsRecords(request):
-    return render(request, 'flights-records.html')
+    return render(request, "flights-records.html")
+
 
 def flightRecordInfo(request):
-    return render(request, 'flight-record-info.html')
+    return render(request, "flight-record-info.html")
+
 
 def flightRegistration(request):
-    return render(request, 'flight-registration.html')
+
+    context = {}
+
+    # create object of form
+    form = FlightForm(request.POST or None, request.FILES or None)
+
+    print(form)
+
+    # check if form data is valid
+    if form.is_valid():
+        # save the form data to model
+        form.save()
+        response = {
+            "id": form.cleaned_data["flightCode"],
+            "success": True,
+            "error": None,
+        }
+        print("form valid")
+        print("myresponse", response)
+        return HttpResponse(json.dumps(response))
+    elif request.method == "POST":
+        print("form invalid", form.data)
+        response = {
+            "id": form.data["flightCode"],
+            "success": False,
+            "error": form.errors.as_json(),
+        }
+        print("myresponse", response)
+        return HttpResponse(json.dumps(response))
+
+    context["form"] = form
+
+    return render(request, "flight-registration.html", context)
