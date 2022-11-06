f1fcd4a8b5933d64304bdabd3e9dfe69eecfbe14
LPFreitas <lfreitasp2001@gmail.com>
2022-11-06 10:19:48 -0300

    ADD: flight creation and deletion

--- src/project/urls.py
+++ src/project/urls.py
@@ -20,17 +20,39 @@ from monitoring import views
 urlpatterns = [
     # Disabled admin site for the purpose of evaluation
     # path('admin/', admin.site.urls),
-    path('', views.index, name='login'),
-    path('monitoring/dashboard', views.dashboard, name='dashboard'),
-    path('monitoring/flight-info', views.flightInfo, name='flight'),
-    path('report', views.chooseReport, name='chooseReport'),
-    path('report/general', views.generalReport, name='generalReport'),
-    path('report/specific', views.specificReport, name='specificReport'),
-    path('routes-and-flights', views.routesAndFlights, name='routesAndFlights'),
-    path('routes-and-flights/routes-records', views.routesRecords, name='routesRecords'),
-    path('routes-and-flights/routes-records/info/<str:flightCode>/', views.routeInfo, name='routeInfo'),
-    path('routes-and-flights/routes-records/register', views.routeRegistration, name='routeRegistration'),
-    path('routes-and-flights/flights-records', views.flightsRecords, name='flightsRecords'),
-    path('routes-and-flights/flights-records/register', views.flightRegistration, name='flightRegistration'),
-    path('routes-and-flights/flights-records/flights-record-info', views.flightRecordInfo, name='flightRecordInfo'),
+    path("", views.index, name="login"),
+    path("monitoring/dashboard", views.dashboard, name="dashboard"),
+    path("monitoring/flight-info", views.flightInfo, name="flight"),
+    path("report", views.chooseReport, name="chooseReport"),
+    path("report/general", views.generalReport, name="generalReport"),
+    path("report/specific", views.specificReport, name="specificReport"),
+    path("routes-and-flights", views.routesAndFlights, name="routesAndFlights"),
+    path(
+        "routes-and-flights/routes-records", views.routesRecords, name="routesRecords"
+    ),
+    path(
+        "routes-and-flights/routes-records/info/<str:flightCode>/",
+        views.routeInfo,
+        name="routeInfo",
+    ),
+    path(
+        "routes-and-flights/routes-records/register",
+        views.routeRegistration,
+        name="routeRegistration",
+    ),
+    path(
+        "routes-and-flights/flights-records",
+        views.flightsRecords,
+        name="flightsRecords",
+    ),
+    path(
+        "routes-and-flights/flights-records/register",
+        views.flightRegistration,
+        name="flightRegistration",
+    ),
+    path(
+        "routes-and-flights/flights-records/flights-record-info/<str:route>/<str:scheduledDate>/",
+        views.flightRecordInfo,
+        name="flightRecordInfo",
+    ),
 ]
